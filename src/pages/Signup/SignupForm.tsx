a64209495f9aaa188233370a8d7ebbfec29d2bb9
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2022-04-20 13:52:44 -0400

    adds axios method to call back-end api

--- src/pages/Signup/SignupForm.tsx
+++ src/pages/Signup/SignupForm.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import { StyledButton, StyledLink, StyledPhoneInput } from "./elements";
+import { StyledButton, StyledPhoneInput } from "./elements";
 import { CustomTextField } from "../../components";
 import { yupResolver } from "@hookform/resolvers/yup";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, FieldValues } from "react-hook-form";
 import * as yup from "yup";
 import "react-phone-input-2/lib/material.css";
+import axios from "axios";
 
 const validationSchema = yup.object({
   firstName: yup.string().required("Required*"),
@@ -34,16 +37,20 @@ const lowercaseFirstLetter = (text: string) => {
 };
 
 export const SignupForm: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const {
     control,
     formState: { errors, isValid },
+    handleSubmit,
   } = useForm({
     resolver: yupResolver(validationSchema),
     mode: "onChange",
     reValidateMode: "onChange",
   });
+
+  const navigate = useNavigate();
+
   const { firstName, lastName, email, phoneNumber, password } = errors;
-  console.log("PASSWORD", password);
 
   const phoneInputStyle = {
     width: "100%",
@@ -54,77 +61,91 @@ export const SignupForm: React.FC = () => {
     boxShadow: "none",
   };
 
+  const onSubmit = (formValues: FieldValues) => {
+    setLoading(true);
+    axios
+      .post("", { data: formValues })
+      .then((response) => {
+        setLoading(false);
+        navigate("detail", { replace: true, state: formValues });
+        // console.log("response", response);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <CustomTextField
-          label="First Name"
-          control={control}
-          fieldName="firstName"
-          textParser={capitalizeFirstLetter}
-          error={firstName}
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <CustomTextField
-          label="Last Name"
-          control={control}
-          fieldName="lastName"
-          textParser={capitalizeFirstLetter}
-          error={lastName}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          name="phoneNumber"
-          control={control}
-          render={({ field }) => (
-            <StyledPhoneInput
-              inputStyle={phoneInputStyle}
-              error={Boolean(phoneNumber?.message)}
-              country="us"
-              specialLabel=""
-              onlyCountries={["us"]}
-              disableDropdown
-              disableCountryCode
-              inputProps={{ placeholder: "Phone number" }}
-              {...field}
-            />
-          )}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <CustomTextField
-          label="Email"
-          fullWidth
-          textParser={lowercaseFirstLetter}
-          control={control}
-          fieldName="email"
-          error={email}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <CustomTextField
-          label="Password"
-          fullWidth
-          control={control}
-          fieldName="password"
-          error={password}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <StyledLink to="detail" state={{ testData: "test" }}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <CustomTextField
+            label="First Name"
+            control={control}
+            fieldName="firstName"
+            textParser={capitalizeFirstLetter}
+            error={firstName}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <CustomTextField
+            label="Last Name"
+            control={control}
+            fieldName="lastName"
+            textParser={capitalizeFirstLetter}
+            error={lastName}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Controller
+            name="phoneNumber"
+            control={control}
+            render={({ field }) => (
+              <StyledPhoneInput
+                inputStyle={phoneInputStyle}
+                error={Boolean(phoneNumber?.message)}
+                country="us"
+                specialLabel=""
+                onlyCountries={["us"]}
+                disableDropdown
+                disableCountryCode
+                inputProps={{ placeholder: "Phone number" }}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CustomTextField
+            label="Email"
+            fullWidth
+            textParser={lowercaseFirstLetter}
+            control={control}
+            fieldName="email"
+            error={email}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CustomTextField
+            label="Password"
+            fullWidth
+            control={control}
+            fieldName="password"
+            error={password}
+          />
+        </Grid>
+        <Grid item xs={12}>
           <StyledButton
+            type="submit"
             fullWidth
             variant="contained"
             bgcolor={isValid ? "#01e0b1" : ""}
             size="large"
           >
-            NEXT
-            {/* <CircularProgress color="inherit" size={24} />  */}
+            {!loading ? "NEXT" : <CircularProgress color="inherit" size={24} />}
           </StyledButton>
-        </StyledLink>
+        </Grid>
       </Grid>
-    </Grid>
+    </form>
   );
 };
--- src/pages/Signup/elements.tsx
+++ src/pages/Signup/elements.tsx
@@ -1,6 +1,5 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
 import PhoneInput, { PhoneInputProps } from "react-phone-input-2";
 
 export const StyledButton = styled(Button)<ButtonProps & { bgcolor?: string }>`
@@ -11,10 +10,6 @@ export const StyledButton = styled(Button)<ButtonProps & { bgcolor?: string }>`
   :hover {
     background-color: #01e0b1;
   }
-`;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
 `;
 
 export const StyledPhoneInput = styled(PhoneInput)<
